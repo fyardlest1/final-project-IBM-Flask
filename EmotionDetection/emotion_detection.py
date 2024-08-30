1c598fef17cec79f89cc6228b70cd31fa03c7910
Yardley <fyardlest1@gmail.com>
2024-08-30 17:30:08 -0500

    testing IBM Code

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,6 +1,7 @@
 import requests  
 import json
 
+
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     
@@ -60,3 +61,41 @@ def emotion_detector(text_to_analyze):
 # from EmotionDetection.emotion_detection import emotion_detector
 # emotion_detector('I hate working long hours')
 # import unittest
+
+
+# IBM code:
+
+# def emotion_detector(text_to_analyse):
+
+#     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+#     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+#     myobj = {"raw_document": { "text": text_to_analyse } }
+#     response = requests.post(url, json = myobj, headers = headers)
+#     res = json.loads(response.text)
+#     formatted_response = res['emotionPredictions'][0]['emotion']
+#     dominant_emotion = max(formatted_response, key = lambda x: formatted_response[x])
+#     formatted_response['dominant_dictionary'] = dominant_emotion
+#     return formatted_response 
+
+# def emotion_detector(text_to_analyse):
+
+#     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+#     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+#     myobj = {"raw_document": { "text": text_to_analyse } }
+#     response = requests.post(url, json = myobj, headers = headers)
+#     status_code = response.status_code
+    
+#     if status_code == 400:
+#         formatted_output = { 'anger': None,
+#                              'disgust': None,
+#                              'fear': None,
+#                              'joy': None,
+#                              'sadness': None,
+#                              'dominant_emotion': None }
+#     else:
+#         res = json.loads(response.text)
+#         formatted_output = res[‘emotionPredictions’][0][‘emotion’]
+#         dominant_emotion = max(formatted_response, key = lambda x: formatted_response[x])
+#         formatted_response[‘dominant_dictionary’] = dominant_emotion
+
+#     return formatted_response 
